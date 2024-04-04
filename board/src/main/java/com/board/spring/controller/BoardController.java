2566848da997c600115ba7771207e1e0444576b0
PC_1M <PC_1M@0759>
2024-04-04 09:22:08 +0900

    Json 형식으로 데이터 출력 테스트

--- board/src/main/java/com/board/spring/controller/BoardController.java
+++ board/src/main/java/com/board/spring/controller/BoardController.java
@@ -1,5 +1,10 @@
 package com.board.spring.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,8 +15,17 @@ import jakarta.servlet.http.HttpServletRequest;
 public class BoardController {
 	@GetMapping("/board")
 	@ResponseBody
-	public String boardHome(HttpServletRequest request) {
-		return "board";
+	public List<Map<String, Object>> boardHome(HttpServletRequest request) {
+		List<Map<String, Object>> members = new ArrayList<>();
+		
+		for (int i = 1; i <= 20; i++) {
+			Map<String, Object> member = new HashMap<>();
+			member.put("id", i);
+			member.put("name", i + "번 Test");
+			member.put("age", 10+i);
+			members.add(member);
+		}
+		return members;
 	}
 
 }
---
+++ board/src/main/java/com/board/spring/entity/BoardEntity.java
@@ -0,0 +1,14 @@
+package com.board.spring.entity;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@EntityScan
+@NoArgsConstructor
+public class BoardEntity {
+	
+
+}
